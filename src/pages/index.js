710a21e82f04b59282bc8a6c0e995182a932aa29
hendraluntungan <hendraluntungan29@gmail.com>
2023-05-08 03:31:34 +0800

    memperbaiki bug

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,29 @@ import PagesPulauLembeh from './PagesPulauLembeh';
 import PagesPantaiPall from './PagesPantaiPall';
 import Home from './Home';
 import PagesBunaken from './PagesBunaken';
+import SignUp from './signup';
+import SplashScreen from './splashscreen';
 import SignIn from './signin';
+import PagesLahendong from './PagesLahendong';
+import PagesBentengMoraya from './PagesBentengMoraya';
+import PagesBukitDoa from './PagesBukitDoa';
+import PagesTamanNasional from './PageTamanNasional';
+import PagesPulauLihaga from './PagesPulauLihaga';
+import PagesBukitKasih from './PagesBukitKasih';
+
 export {
+  PagesBukitKasih,
+  PagesPulauLihaga,
+  PagesTamanNasional,
+  PagesBukitDoa,
+  PagesBentengMoraya,
+  PagesLahendong,
   PagesDanauLinow,
   PagesPulauLembeh,
   SignIn,
   PagesPantaiPall,
   PagesBunaken,
   Home,
+  SignUp,
+  SplashScreen,
 };
--- src/pages/splashscreen/index.js
+++ src/pages/splashscreen/index.js
@@ -1,6 +1,33 @@
-import PagesDanauLinow from './PagesDanauLinow';
-import PagesPulauLembeh from './PagesPulauLembeh';
-import SignIn from './signin';
-import SignUp from './signup';
-import SplashScreen from './splashscreen';
-export {PagesDanauLinow, PagesPulauLembeh, SignIn, SignUp, SplashScreen};
+import {StyleSheet, Text, View} from 'react-native';
+import React, {useEffect} from 'react';
+import {Logo} from '../../assets';
+
+const SplashScreen = ({navigation}) => {
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.replace('SignIn');
+    }, 3000);
+  }, []);
+  return (
+    <View style={styles.container}>
+      <Logo />
+      <Text style={styles.text}>Money Tracker</Text>
+    </View>
+  );
+};
+
+export default SplashScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#92F0F6',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 32,
+    fontFamily: 'Poppins-Medium',
+    color: 'black',
+  },
+});
